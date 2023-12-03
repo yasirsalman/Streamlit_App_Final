a1d42ef9abf2390b3a3eb69ffdaad539b56dcd88
Yasir <yasir_salman@outlook.com>
2023-12-03 00:29:34 -0700

    Refactor create_plot function to separate historical and forecasted data

--- app_v4_final.py
+++ app_v4_final.py
@@ -2087,31 +2087,55 @@ with tab3:
 
 
     @st.cache_data
+    # def create_plot(filtered_df, forecasts):
+    #     all_years = list(filtered_df['ROLL_YEAR'].unique())
+    #     forecast_years = [2024, 2025, 2026, 2027, 2028]
+    #     all_years.extend(forecast_years)
+    #     all_years = sorted(set(all_years))
+
+    #     # Append the forecast data to the filtered dataframe
+    #     for comm_name, forecast_df in forecasts.items():
+    #         if comm_name in filtered_df['COMM_NAME'].unique():
+    #             filtered_df = pd.concat([filtered_df, forecast_df], ignore_index=True)
+
+    #     # Generate the plot
+    #     fig = px.line(filtered_df, x='ROLL_YEAR', y='ASSESSED_VALUE', color='COMM_NAME',
+    #                 labels={'ROLL_YEAR': 'Year', 'ASSESSED_VALUE': 'Average Assessed Value', 'COMM_NAME': 'Community'},
+    #                 title='Average Assessed Value per Community Over Years')
+
+    #     # Differentiate forecasted data with a dotted line
+    #     for comm_name in forecasts.keys():
+    #         forecast_data = filtered_df[(filtered_df['COMM_NAME'] == comm_name) & (filtered_df['is_forecast'] == True)]
+    #         if not forecast_data.empty:
+    #             fig.add_scatter(x=forecast_data['ROLL_YEAR'], y=forecast_data['ASSESSED_VALUE'], mode='lines',
+    #                             line=dict(dash='dot'), name=f"{comm_name} Forecast")
+
+    #     fig.update_layout(title_text="Assessed Value", title_x=0.3, width=800, height=600,
+    #                     xaxis=dict(tickmode='array', tickvals=all_years, ticktext=[str(year) for year in all_years]))
+    #     return fig
+
     def create_plot(filtered_df, forecasts):
         all_years = list(filtered_df['ROLL_YEAR'].unique())
         forecast_years = [2024, 2025, 2026, 2027, 2028]
         all_years.extend(forecast_years)
         all_years = sorted(set(all_years))
 
-        # Append the forecast data to the filtered dataframe
-        for comm_name, forecast_df in forecasts.items():
-            if comm_name in filtered_df['COMM_NAME'].unique():
-                filtered_df = pd.concat([filtered_df, forecast_df], ignore_index=True)
-
-        # Generate the plot
-        fig = px.line(filtered_df, x='ROLL_YEAR', y='ASSESSED_VALUE', color='COMM_NAME',
+        # Generate the plot for historical data
+        fig = px.line(filtered_df[filtered_df['is_forecast'] != True], 
+                    x='ROLL_YEAR', y='ASSESSED_VALUE', color='COMM_NAME',
                     labels={'ROLL_YEAR': 'Year', 'ASSESSED_VALUE': 'Average Assessed Value', 'COMM_NAME': 'Community'},
                     title='Average Assessed Value per Community Over Years')
 
         # Differentiate forecasted data with a dotted line
-        for comm_name in forecasts.keys():
-            forecast_data = filtered_df[(filtered_df['COMM_NAME'] == comm_name) & (filtered_df['is_forecast'] == True)]
+        for comm_name, forecast_df in forecasts.items():
+            forecast_data = forecast_df[forecast_df['COMM_NAME'] == comm_name]
             if not forecast_data.empty:
                 fig.add_scatter(x=forecast_data['ROLL_YEAR'], y=forecast_data['ASSESSED_VALUE'], mode='lines',
                                 line=dict(dash='dot'), name=f"{comm_name} Forecast")
 
-        fig.update_layout(title_text="Assessed Value", title_x=0.3, width=800, height=600,
+        fig.update_layout(title_text="Assessed Value", title_x=0.5, width=800, height=600,
                         xaxis=dict(tickmode='array', tickvals=all_years, ticktext=[str(year) for year in all_years]))
+
         return fig
 
     # @st.cache_data
