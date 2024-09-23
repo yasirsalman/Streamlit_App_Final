cde710c568d954d245a283bebf2d6d28efad4f79
YS <77942868+yasirsalman@users.noreply.github.com>
2024-09-22 20:45:32 -0600

    Remove unnecessary code for clearing cache

--- app_v4_final.py
+++ app_v4_final.py
@@ -2029,7 +2029,6 @@ with tab3:
         return df, se_df, gdf, gdf_projected
 
     df, se_df, gdf, gdf_projected = load_data()
-    st.legacy_caching.clear_cache()
 
     def generate_forecasts(df):
         from sklearn.linear_model import LinearRegression
